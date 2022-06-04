74facee69fe5f229dc6820557cff41b30a049f05
afsal <mycodehack@gmail.com>
2022-06-04 21:47:40 +0300

    all sum added for vertical tree sum

--- python/Interview/vertical_sum_tree.py
+++ python/Interview/vertical_sum_tree.py
@@ -5,9 +5,10 @@ class Node:
         self.left = None
         self.right = None
         self.data = data
-        
+    
 class Solution:
     sum = 0
+    map = {}
     def vertical_sum(self,node: Node , level, target):
         if node is None: return
 
@@ -18,7 +19,19 @@ class Solution:
 
         self.vertical_sum(node.right , level+1 ,target)
 
+    def vertical_sum_all(self,node: Node , level):
+        if node is None: return
 
+        if level in self.map:
+            self.map[level] + self.map[level] + node.data
+        else:
+            self.map[level] = node.data
+        
+        self.sum = self.sum + node.data
+            
+        self.vertical_sum_all(node.left , level-1)
+
+        self.vertical_sum_all(node.right , level+1)    
 
 root = Node(1)
 root.left = Node(2);
@@ -33,3 +46,6 @@ root.right.right.right = Node(9);
 driver = Solution()
 driver.vertical_sum(root , 0 , 3)
 print(driver.sum)
+
+driver.vertical_sum_all(root , 0)
+print(driver.map)
