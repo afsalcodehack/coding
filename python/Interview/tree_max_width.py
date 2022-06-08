c0faebd96c844ffcdec617d0f8455bc5120922ca
afsal <afsal.aimit.2010@hotmail.com>
2022-06-08 16:48:45 +0300

    max width prblm solved

--- python/Interview/tree_max_width.py
+++ python/Interview/tree_max_width.py
@@ -12,19 +12,26 @@ class Solution:
 
     def tree_max_width(self,node: Node , level):
         max_width = 0
+        width_count = 0
         q1 = Queue()
         marker = Node(-1)
         q1.put(node)
         q1.put(marker)
         while not q1.empty():
             current_node = q1.get()
-            if(current_node == marker):
+            if current_node == marker:
+                if width_count > max_width: max_width = width_count
+                width_count = 0
+            if current_node == marker and not q1.empty():
                 q1.put(marker)
-                print("marker found")
             else:
+                width_count = width_count + 1
                 print(current_node.data)
-                q1.put(current_node.left)
-                q1.put(current_node.right)
+                if current_node.left is not None:
+                    q1.put(current_node.left)
+                if current_node.right is not None:
+                    q1.put(current_node.right)
+        print("max width = " , max_width)
 
 
 
@@ -35,12 +42,11 @@ root.right = Node(3);
 root.left.left = Node(4);
 root.left.right = Node(5);
 root.right.left = Node(6);
-root.right.right = Node(7);
-root.right.left.right = Node(8);
-root.right.right.right = Node(9);
-root.right.right.right.right = Node(12);
+# root.right.right = Node(7);
+# root.right.left.right = Node(8);
+# root.right.right.right = Node(9);
+# root.right.right.right.right = Node(12);
 
 driver = Solution()
 driver.tree_max_width(root , 1)
-print(driver.max_width)
 
