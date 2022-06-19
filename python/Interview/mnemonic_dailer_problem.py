0e189381e5f07fc77358b0920a02ef64a6fea8ea
afsal <mycodehack@gmail.com>
2022-06-19 17:21:04 +0300

    dailer problem

---
+++ python/Interview/mnemonic_dailer_problem.py
@@ -0,0 +1,18 @@
+class Solution:
+    keys  = {1 : "abc", 2 : "def", 3: "ghi"}
+    def findDailer(self ,result,  level, nums):
+
+        if len(nums) == level:
+            print(result)
+            return
+        
+        for i in self.keys[int(nums[level])]:
+            result = result + i
+            self.findDailer(result, level + 1 , nums)
+            result = result[:-1]
+
+
+
+driver  = Solution()
+
+driver.findDailer('',0, '123')  
